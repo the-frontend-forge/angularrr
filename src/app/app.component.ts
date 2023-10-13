1ad3bd5b0b9dc73cc8bed88f4cf7a4bec1eac821
Tyler Nielsen <tynielsen@gmail.com>
2023-10-12 20:34:29 -0600

    adding images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    NxWelcomeComponent,
+    RouterModule
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
